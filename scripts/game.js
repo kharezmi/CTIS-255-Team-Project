3d07e9591ecec141176acec3cdec034fe2ad11e5
Strayberry9 <109548625+strayberry-97@users.noreply.github.com>
2025-05-11 20:10:15 +0300

    Game update
    Added heart spawning in intervals with checking distance between them, working on implementing shooting

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,10 @@
-const SIZE = 8;
-const COLORS = ["pink", "purple", "gold"];
+const SIZE = 8; //number of hearts
+const COLORS = ["pink", "purple", "gold"]; //heart colors
+const MIN_SPACING = 50; // min distance between hearts
+
 
 let score = 0;
 let hearts = new Array(SIZE);
-
 
 
 class Heart {
@@ -16,48 +17,147 @@ class Heart {
     }
 }
 
-for (let i = 0; i < SIZE; i++) {
-    const x = Math.floor(Math.random() * (1250 - 770 + 1)) + 770; // [770, 1250]
-    const y = Math.floor(Math.random() * (620 - 310 + 1)) + 310;  // [310, 620]
-    const rand = Math.floor(Math.random() * 3);
-    const color = COLORS[rand];
-    const value = rand === 0 ? 5 : rand === 1 ? 10 : 20; // pink=5, purple=10, gold=20
+function shoot() {
+    let bowPos = $("#bow").offset();
+    let x = bowPos.left;
+    let y = bowPos.top;
 
-    hearts[i] = new Heart(x, y, color, false, value);
+    let arrow_img = document.createElement("img");
+    arrow_img.src = "images/arrow_heart.svg";
+    arrow_img.style.position = "absolute";
+    arrow_img.style.width = "21";
+    arrow_img.style.height = "18";
+    arrow_img.style.left = x + "px";
+    arrow_img.style.top = y + "px";
+
+    arrows.push({x, y, angle : angle, img : arrow_img});
+    $("#game").append(arrow_img)
 }
 
-$(document).ready(function () {
-    $("#game").on("click", start_game);
-    function start_game(){
-        let game_timer = window.setInterval(function () {
-            let timer = parseInt($("#sec").html());
-            if (timer > 0){
-                timer--;
-                timer > 9 ? $("#sec").html(timer) : $("#sec").html('0' + timer);
-            }
-            else{
-                clearInterval(game_timer);
-                //done screen insert later
-            }
-                
-            
-        }, 1000);
+let isGameStarted = false;
 
-        let angle = 0;
-        
-        $(window).on("keydown", function(e){
-            if (e.key == "ArrowLeft")
-                angle -= 5;
-            else if (e.key == "ArrowRight")
-                angle += 5;
-            angle = Math.min(90, angle);
-            angle = Math.max(-90, angle);
-            $("#bow").css({"transform" : "rotate(" + angle + "deg)", "transform-origin" : "50% 75%"});
+$(document).ready(function () {
+
+    $("#game").on("click", start_game);
+
+
+    function start_game() {
+        if (!isGameStarted) {
+            isGameStarted = true;
+            $("#game").off("click")
+
+            let arrows = [];
+
+
+
+            for (let i = 0; i < SIZE; i++) {
+                let tooClose = false;
+                let retries = 0;
+                let x, y;
+                do {
+                    x = Math.floor(Math.random() * (480 - 50 + 1)) + 40; // [50, 510]
+                    y = Math.floor(Math.random() * (350 - 100 + 1)) + 100;  // [100, 400]
+                    for (let j = 0; j < SIZE; j++) {
+                        if (j < i && hearts[j]) {
+                            let distance = Math.sqrt((x - hearts[j].x) ** 2 + (y - hearts[j].y) ** 2);
+                            if (i != j && distance <= MIN_SPACING)
+                                tooClose = true;
+                        }
+
+                    }
+                    retries++;
+                } while (retries < 1000 && tooClose);
+
+                const rand = Math.floor(Math.random() * 3);
+                const color = COLORS[rand];
+                const value = rand === 0 ? 5 : rand === 1 ? 10 : 20; // pink=5, purple=10, gold=20
+
+                hearts[i] = new Heart(x, y, color, false, value);
+
+                let img = document.createElement("img");
+                img.src = "images/heart1.svg";
+                img.style.position = "absolute";
+                img.style.left = hearts[i].x + "px";
+                img.style.top = hearts[i].y + "px";
+                img.id = "heart-" + i;
+                img.className = "heart";
+                $("#game").append(img);
+            }
+
+            let arrow_timer = setInterval(function () {
+                for(let arrow of arrows){
+                    arrow.x += 5 * Math.cos(arrow.angle * Math.PI / 180);
+                    arrow.y -= 5 * Math.sin(arrow.angle * Math.PI / 180);
+                    arrow.element.css({ left: arrow.x + "px", top: arrow.y + "px" });
+                    if(arrow.x < 0 || arrow.x > 480 || arrow.y < 0 || arrow.y > 350){
+                        arrow.img.remove();
+                        arrows.splice(arrow);
+                    }
+                }
+            }, 50);
+
+            let heart_timer = window.setInterval(function () {
+                for (let i = 0; i < SIZE; i++) {
+                    let tooClose = false;
+                    let retries = 0;
+                    do {
+                        hearts[i].x = Math.floor(Math.random() * (480 - 50 + 1)) + 40;
+                        hearts[i].y = Math.floor(Math.random() * (350 - 100 + 1)) + 100;
+                        for (let j = 0; j < SIZE; j++) {
+                            if (j != i && hearts[j]) {
+                                let distance = Math.sqrt((hearts[i].x - hearts[j].x) ** 2 + (hearts[i].y - hearts[j].y) ** 2);
+                                if (i != j && distance <= MIN_SPACING)
+                                    tooClose = true;
+                            }
+                        }
+                        retries++;
+                    } while (retries < 1000 && tooClose);
+
+                    $("#heart-" + i).css("left", hearts[i].x + "px");
+                    $("#heart-" + i).css("top", hearts[i].y + "px");
+                    //change hearts' colors
+                }
+            }, 5000);
+
+
+            let game_timer = window.setInterval(function () {
+                let timer = parseInt($("#sec").html());
+                if (timer > 0) {
+                    timer--;
+                    timer > 9 ? $("#sec").html(timer) : $("#sec").html('0' + timer);
+                }
+                else {
+                    isGameStarted = false;
+                    $("#game").on("click", start_game);
+                    $(".heart").remove();
+                    $("#sec").html('30');
+                    clearInterval(game_timer);
+                    clearInterval(heart_timer);
+                    clearInterval(arrow_timer);
+                    //done screen insert later
+                }
+
+
+            }, 1000);
+
+
+            let angle = 0;
+
+            $(window).on("keydown", function (e) {
+                if (e.key == "ArrowLeft")
+                    angle -= 5;
+                else if (e.key == "ArrowRight")
+                    angle += 5;
+                angle = Math.min(60, angle);
+                angle = Math.max(-60, angle);
+                $("#bow").css({ "transform": "rotate(" + angle + "deg)", "transform-origin": "50% 75%" });
             });
+
+        }
     }
-    
-    
-            
+
+
+
 });
 
 
