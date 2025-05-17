c7b75bbd389776391b58ca0d41a4d18b09809976
Utku Kaya <utkukaya@Utkus-Macbook-Pro.local>
2025-05-17 16:02:46 +0300

    form width fixed

--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,4 @@
-var faculties = [
+const faculties = [
   "Information Systems and Technologies",
   "Tourism and Hotel Management",
   "Architecture",
@@ -28,3 +28,46 @@ var faculties = [
   "Molecular Biology and Genetics",
   "Physics",
 ];
+
+const hobbies = [
+  "Reading",
+  "Painting",
+  "Running",
+  "Gardening",
+  "Cooking",
+  "Playing chess",
+  "Dancing",
+  "Watching movies",
+  "Photography",
+  "Listening to music",
+  "Playing guitar",
+  "Swimming",
+  "Doing yoga",
+  "Cycling",
+  "Mountaineering",
+  "Traveling",
+  "Collecting",
+  "Camping",
+  "Fishing",
+  "Knitting",
+  "Handicrafts",
+  "Ceramics",
+  "Programming",
+  "Birdwatching",
+  "Woodworking",
+  "Basketball",
+  "Football",
+  "Tennis",
+  "Fitness",
+  "Bodybuilding"
+];
+
+const select = document.getElementById("hobbie");
+
+select.addEventListener("change", function() {
+  if (select.value !== "") {
+    select.style.backgroundImage = "none";
+  } else {
+    select.style.backgroundImage = "url('your-image-url.png')";
+  }
+});
