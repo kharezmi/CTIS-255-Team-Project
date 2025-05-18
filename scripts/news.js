7c6fd19aa10d87e30360b218cb68ccd3c716ebca
Strayberry9 <109548625+strayberry-97@users.noreply.github.com>
2025-05-18 18:17:23 +0300

    Added automatic articles from js

---
+++ scripts/news.js
@@ -0,0 +1,22 @@
+$(document).ready(function (){
+    news.forEach(article => {
+        const card=`
+        <article>
+            <div class="article-left article-card">
+                <div class="closer">
+                    <h3>
+                        ${article.title}
+                    </h3>
+                    <p>
+                        ${article.text}
+                    </p>
+                </div>
+                <button>See more</button>
+            </div>
+            <div class="article-image" style="background: url(${article.img}) center;background-position:center; background-size:cover"></div>
+        </article>
+        `;
+        $("#articles").append(card);
+    })
+
+})
