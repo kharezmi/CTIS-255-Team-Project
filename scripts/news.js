92b21b9480dd13bb786ff50f15fb7e4a3d3b374e
Strayberry9 <109548625+strayberry-97@users.noreply.github.com>
2025-05-19 12:24:55 +0300

    Added gallery

--- scripts/news.js
+++ scripts/news.js
@@ -1,13 +1,43 @@
+let lastManualChange = 0;
 let currentIndex = 0;
 
-function updateGallery(){
+function updateGallery() {
     const item = headliners[currentIndex];
-    $("gallery").html(``)
+    $("#gallery").fadeOut(300, () => {
+        $("#gallery").html(`
+            <div id="img" style="background: url(${item.img}) center; background-size: cover"></div>
+            <div id="gallery-right" class="article-card">
+                <div class="closer">
+                    <h3>${item.title}</h3>
+                    <p>${item.text}</p>
+                </div>
+                <button>See more</button>
+            </div>
+        `).fadeIn(300);
+    });
 }
 
-$(document).ready(function (){
+$(document).ready(function () {
+    updateGallery();
+    setInterval(() => {
+        if (Date.now() - lastManualChange > 3000) {
+            currentIndex = (currentIndex + 1) % headliners.length;
+            updateGallery();
+        }
+    }, 3000);
+    $("#left-arrow").click(() => {
+        currentIndex = (currentIndex - 1 + headliners.length) % headliners.length;
+        lastManualChange = Date.now();
+        updateGallery();
+    })
+    $("#right-arrow").click(() => {
+        currentIndex = (currentIndex + 1) % headliners.length;
+        lastManualChange = Date.now();
+        updateGallery();
+    })
+
     news.forEach(article => {
-        const card=`
+        const card = `
         <article>
             <div class="article-left article-card">
                 <div class="closer">
